23d6f8a24bffb5dafe5b1442b0f84f7051e7a7bc
DeadSplicer <zombieishungry@yahoo.com>
2017-04-07 01:45:26 -0400

    Super secret stuff added

--- javaScript/mobileDetector.js
+++ javaScript/mobileDetector.js
@@ -21,3 +21,18 @@ $('.menu_button').on('click', function(){
   $('#mobileMenu').slideToggle(823, "easeOutExpo");
   //alert("click");
 })
+
+//Nothing happens here, so just jog on. Twat.
+var txt = "";
+var currentPage = document.getElementById("active").textContent;
+
+if (currentPage === "Photography"){
+  window.addEventListener("keypress", function(event) {
+    txt += event.code[3];
+    if(txt === "CODE"){
+      window.location = "../../txt/index.html"
+      console.log("worked");
+    }
+  });
+  console.log("Password:");
+}
