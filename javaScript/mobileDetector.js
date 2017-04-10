7d729c0082d9fb3da9e22444a6575b35828533e9
DeadSplicer <zombieishungry@yahoo.com>
2017-04-09 21:04:23 -0400

    added a wider browser compatibility for getting to the secret

--- javaScript/mobileDetector.js
+++ javaScript/mobileDetector.js
@@ -1,6 +1,22 @@
 var isMobile = window.matchMedia("only screen and (max-width: 767px)");
 
+//check if the user is using chrome (used to determine how to handle the input)
+function isChrome() {
+  var isChromium = window.chrome,
+    winNav = window.navigator,
+    vendorName = winNav.vendor,
+    isOpera = winNav.userAgent.indexOf("OPR") > -1,
+    isIEedge = winNav.userAgent.indexOf("Edge") > -1,
+    isIOSChrome = winNav.userAgent.match("CriOS");
 
+  if(isIOSChrome){
+    return true;
+  } else if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false && isIEedge == false) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 if (isMobile.matches) {
   //Conditional script here
@@ -28,10 +44,18 @@ var currentPage = document.getElementById("active").textContent;
 
 if (currentPage === "Photography"){
   window.addEventListener("keypress", function(event) {
-    txt += event.code[3];
-    if(txt === "CODE"){
-      window.location = "../../txt/index.html"
-      console.log("worked");
+    if (isChrome()){
+      console.log("Chrome!")
+      txt += event.code[3];
+      if(txt === "CODE"){
+        window.location = "../../txt/index.html"
+      }
+    }
+    else {
+      txt += event.char.toUpperCase();
+      if(txt === "CODE"){
+        window.location = "../../txt/index.html"
+      }
     }
   });
   console.log("Password:");
