62a5f3624d00701346a50a9940ccd5f5e7291d09
DeadSplicer <zombieishungry@yahoo.com>
2018-03-07 22:14:03 -0500

    testing echo

--- pages/test/send.php
+++ pages/test/send.php
@@ -2,9 +2,9 @@
 $name = $_POST['name'];
 $comment = $_POST['comment'];
 
-$to = "zombieishungry@yahoo.com";
-$subject = "New comment to review for Everthing Rocks!";
-$body = "Text content to review: \n\n $name \n $comment";
+// $to = "zombieishungry@yahoo.com";
+// $subject = "New comment to review for Everthing Rocks!";
+// $body = "Text content to review: \n\n $name \n $comment";
 
 // mail($to,$subject,$body)
 
