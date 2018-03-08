1f97b24f70a0acfe5efc2db2265f78632a1d2d8a
DeadSplicer <zombieishungry@yahoo.com>
2018-03-07 21:52:40 -0500

    undid commenting section out

--- pages/test/send.php
+++ pages/test/send.php
@@ -1,14 +1,14 @@
 <?php
-// $name = $_POST['name'];
-// $comment = $POST['comment'];
-//
-// $to = "zombieishungry@yahoo.com";
-// $subject = "New comment to review for Everthing Rocks!";
-// $body = "Text content to review: \n\n $name \n $comment";
-//
-// mail($to,$subject,$body)
+$name = $_POST['name'];
+$comment = $POST['comment'];
+
+$to = "zombieishungry@yahoo.com";
+$subject = "New comment to review for Everthing Rocks!";
+$body = "Text content to review: \n\n $name \n $comment";
+
+mail($to,$subject,$body)
 
 // echo "message sent";
-phpinfo();
+
 
  ?>
