57966a644a1a0f90d273ce527b35757bb69a792e
DeadSplicer <zombieishungry@yahoo.com>
2020-04-20 20:00:51 -0400

    removed insta switcher

--- javaScript/mobileDetector.js
+++ javaScript/mobileDetector.js
@@ -23,8 +23,8 @@ if (isMobile.matches) {
   $("div#desktopMenu").attr("id", "mobileMenu");
   $("div a.button").attr("class", "mobileButton");
   $(".menu_button").show();
-  $(".mobile_insta").show();
-  $(".desktop_insta").hide();
+  // $(".mobile_insta").show();
+  // $(".desktop_insta").hide();
   $("a").removeClass("filter");
 
 }
@@ -32,8 +32,8 @@ else {
   $("div#desktopMenu").attr("id", "desktopMenu");
   $("div a.mobileButton").attr("class", "button");
     $(".menu_button").hide();
-    $(".mobile_insta").hide();
-    $(".desktop_insta").show();
+    // $(".mobile_insta").hide();
+    // $(".desktop_insta").show();
 }
 
 $('#mobileMenu').hide();
